a40e1137984a582d20f395ca125e929b524d5226
syAnasali <anas67660@gmail.com>
2026-03-03 16:24:38 +0530

    navbar bg fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.screenY > 10);
+      setIsScrolled(window.scrollY > 10);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -28,7 +28,7 @@ export const Navbar = () => {
     <nav
       className={cn(
         "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        isScrolled ? "py-5 bg-background/95 backdrop-blur-md shadow-sm" : "py-5"
       )}
     >
       <div className="container flex items-center justify-between">
